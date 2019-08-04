c95f29442f7b804461d0658fcd2e8fe912e10380
Lyss <lyssfitz@outlook.com>
2019-08-04 14:33:28 +1000

    Add starter code for create-exercise, create-user, edit-exercises and exercises-list components

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import Navbar from "./components/navbar.component";
 import ExercisesList from "./components/exercises-list.component";
-import EditExercise from "./components/edit-exercise.component";
+import EditExercise from "./components/edit-exercises.component";
 import CreateExercise from "./components/create-exercise.component";
 import CreateUser from "./components/create-user.component";
 
